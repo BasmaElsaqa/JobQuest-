d83480ecde0373c96c42efdd65487e511c2dbbe1
BasmaElsaqa <elsaqabasma@gmail.com>
2025-05-11 01:53:05 +0300

    All screens added as ui

--- JobQuest App/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/jobquestbottomnavigation/databinding/FragmentProfileBinding.java
+++ JobQuest App/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/jobquestbottomnavigation/databinding/FragmentProfileBinding.java
@@ -8,16 +8,29 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
+import androidx.viewpager2.widget.ViewPager2;
 import com.example.jobquestbottomnavigation.R;
+import com.google.android.material.tabs.TabLayout;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentProfileBinding implements ViewBinding {
   @NonNull
   private final FrameLayout rootView;
 
-  private FragmentProfileBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final TabLayout statusTabs;
+
+  @NonNull
+  public final ViewPager2 viewPager;
+
+  private FragmentProfileBinding(@NonNull FrameLayout rootView, @NonNull TabLayout statusTabs,
+      @NonNull ViewPager2 viewPager) {
     this.rootView = rootView;
+    this.statusTabs = statusTabs;
+    this.viewPager = viewPager;
   }
 
   @Override
@@ -43,10 +56,25 @@ public final class FragmentProfileBinding implements ViewBinding {
 
   @NonNull
   public static FragmentProfileBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.status_tabs;
+      TabLayout statusTabs = ViewBindings.findChildViewById(rootView, id);
+      if (statusTabs == null) {
+        break missingId;
+      }
 
-    return new FragmentProfileBinding((FrameLayout) rootView);
+      id = R.id.view_pager;
+      ViewPager2 viewPager = ViewBindings.findChildViewById(rootView, id);
+      if (viewPager == null) {
+        break missingId;
+      }
+
+      return new FragmentProfileBinding((FrameLayout) rootView, statusTabs, viewPager);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
